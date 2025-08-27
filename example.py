f671c5ddbf8dc67583d47dee876069a002aecbcc
Manish Aravindh <manisharavindh2007@gmail.com>
2025-08-27 10:51:41 +0530

    game info message

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ import make_24
 
 def main():
     make_24.make_24(4, 7, 2, 1)
-    print()
     make_24.make_24(4, 7, 2, 1, goal=32)
 
 
--- make_24/api.py
+++ make_24/api.py
@@ -6,5 +6,6 @@ from make_24.gametree import GameDriver
 
 
 def make_24(*args, goal=24):
+    print(f"\nplaying with {args} goal is {goal}")
     game = GameDriver(*args, goal=goal)
     game.run()
